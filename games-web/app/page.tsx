6618fb683f2bcf60698e46e3b453e1ca6338a3eb
NikolaGrigorov0 <129086579+NikolaGrigorov0@users.noreply.github.com>
2024-01-30 18:35:10 +0200

    new navigation

--- games-web/app/page.tsx
+++ games-web/app/page.tsx
@@ -2,9 +2,9 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <nav>
+  
     <h1 className="text-6xl">Game Web</h1>
-        </nav>
+  
     </main>
   );
 }
