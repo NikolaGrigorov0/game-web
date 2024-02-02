4a8ba19330215c25aa4accfd663a88c1c066534f
NikolaGrigorov0 <129086579+NikolaGrigorov0@users.noreply.github.com>
2024-02-03 00:05:33 +0200

    reinstalled project

---
+++ games-app/app/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+export default function Home() {
+  return (
+   <main>
+    <h2>Games Web</h2>
+    <button onClick={() => console.log("clicked")}>button
+
+    </button>
+   </main>
+  );
+}
